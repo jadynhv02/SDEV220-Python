73202daf1f9914887643a908641301373ec6b636
jadynhv02 <130079363+jadynhv02@users.noreply.github.com>
2024-01-25 10:59:40 -0500

    Add files via upload

---
+++ M01 Programming Assignment.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Name: Vessels, Jadyn
+Date: 01/21/2024
+Course: SDEV 220
+Instructor: Francis,Christopher
+Assignment: M01 Programming Assignment
+"""
+
+seconds = 60
+minutes = 60
+hours = 24
+
+seconds_per_hour = seconds * minutes
+seconds_per_day = seconds_per_hour * hours
+
+float_point = seconds_per_day/seconds_per_hour
+int_div = seconds_per_day//seconds_per_hour
+
+
+print(f"Seconds per hour: {seconds_per_hour}\n")
+print(f"Seconds per day: {seconds_per_day}\n")
+
+print (f"Floating-point Division: {float_point}\n")
+print (f"Integer Division: {int_div}\n")
+
+## 3.6 Question: The floating point value and integer value are different because of how it
+##               is stored. It takes 64-bits to store a float and 32-bits to store an integer.
